52ca093ef0f3169dfbb49eee5b37a58b5c8dc7ff
Sarah Maier <97710343+sarmaier@users.noreply.github.com>
2023-12-13 18:56:19 -0500

    Add files via upload
    adding FEATHER folder, with modified attributed graph class in feather.py

---
+++ FEATHER/src/main.py
@@ -0,0 +1,38 @@
+"""Running FEATHER."""
+
+from utils import tab_printer
+from feather import FEATHER, FEATHERG, FEATHERG_att
+from param_parser import parameter_parser
+from utils import load_graph, load_features, load_graphs, save_embedding
+
+def main(args):
+    """
+    Characteristic function embedding wrapper.
+    :param args: Arguments object parsed up.
+    """
+    if args.model_type == "FEATHER":
+        print("\nFitting a node embedding.\n")
+        graph = load_graph(args.graph_input)
+        features = load_features(args.feature_input)
+        model = FEATHER()
+        model.fit(graph, features)
+    elif args.model_type == "FEATHER-G":
+        print("\nFitting a graph level embedding.\n")
+        graphs = load_graphs(args.graphs_input)
+        model = FEATHERG()
+        model.fit(graphs)
+    elif args.model_type == "FEATHER-G-att":
+        print("\nFitting a graph level embedding with node attributes.\n")
+        graph = load_graph(args.graph_input)
+        features = load_features(args.feature_input)
+        model = FEATHERG_att()
+        model.fit(graph, features)
+    else:
+        quit()
+    X = model.get_embedding()
+    save_embedding(X, args.output)
+
+if __name__ == "__main__":
+    args = parameter_parser()
+    tab_printer(args)
+    main(args)
