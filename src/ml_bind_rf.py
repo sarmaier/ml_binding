dd5dd9608fe95b71e317b339675df7b910d690b3
sarmaier <sarmaier@iu.edu>
2023-12-14 12:11:47 -0500

    adding rf run file

---
+++ src/ml_bind_rf.py
@@ -0,0 +1,9 @@
+import os
+from datetime import date
+import glob
+import json_numpy
+from ligand_block import LigandBlock
+from gbsa_block import GbsaComplexBlock
+from interaction_gbsa_block import GbsaInteraction
+import logging
+
